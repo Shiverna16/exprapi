8d90858e9e2b0f6ef1446d14f38addc4907d7035
Mohanavelu <mohanaveluk@gmail.com>
2021-02-11 19:40:25 -0600

    updated env file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var http = require('http');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
